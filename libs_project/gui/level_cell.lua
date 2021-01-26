e4f02628a37b17080915654c97da2702e7673111
d954mas <d954mas@gmail.com>
2021-01-26 20:14:42 +0300

    правки в экран меню

--- libs_project/gui/level_cell.lua
+++ libs_project/gui/level_cell.lua
@@ -2,7 +2,14 @@ local COMMON = require "libs.common"
 local LEVELS = require "assets.levels.levels"
 local WORLD = require "world.world"
 local SM = require "libs_project.sm"
+local COLOR_HELPER = require "richtext.color"
 local ButtonScale = require "libs_project.gui.button_scale"
+
+local COLORS = {
+    CURRENT = COLOR_HELPER.parse_hex("#6b8e23"),
+    CLOSED = COLOR_HELPER.parse_hex("#472A15"), --COLOR_HELPER.parse_hex("#999999"),
+    OPENED = COLOR_HELPER.parse_hex("#472A15")
+}
 
 ---@class LevelCellGui
 local LevelCell = COMMON.class("LevelCellGui")
@@ -12,6 +19,7 @@ function LevelCell:initialize(root_name)
         root = gui.get_node(root_name .. "/root"),
         bg = gui.get_node(root_name .. "/bg"),
         icon = gui.get_node(root_name .. "/icon"),
+        lock = gui.get_node(root_name .. "/lock"),
         stars = {
             gui.get_node(root_name .. "/star_1"),
             gui.get_node(root_name .. "/star_2"),
@@ -31,20 +39,29 @@ end
 function LevelCell:level_set_idx(lvl_idx)
     checks("?", "number")
     self.level = assert(LEVELS.get_by_idx(lvl_idx))
-    local stars = WORLD.storage:level_get_stars(lvl_idx)
-    for i = 1, 3 do
-        gui.play_flipbook(self.vh.stars[i], COMMON.HASHES.hash((i >= stars) and "icon_star_on" or "icon_star_off"))
-    end
     gui.play_flipbook(self.vh.icon, COMMON.HASHES.hash(self.level.regions[1].art))
+
     self:on_storage_changed()
 end
 
 function LevelCell:on_storage_changed()
-    if(self.level == WORLD:level_get_current())then
-        gui.set_color(self.vh.bg,vmath.vector4(1,1,1,1))
-    else
-        gui.set_color(self.vh.bg,vmath.vector4(0.44,0.44,0.44,1))
+    local opened = WORLD:level_can_play(self.level.idx)
+    for i = 1, 3 do
+        gui.set_enabled(self.vh.stars[i], opened)
     end
+    local stars = WORLD.storage:level_get_stars(self.level.idx)
+    for i = 1, 3 do
+        gui.play_flipbook(self.vh.stars[i], COMMON.HASHES.hash((i <= stars) and "icon_star_on" or "icon_star_off"))
+    end
+
+    gui.set_enabled(self.vh.lock, not opened)
+
+    if (self.level == WORLD:level_get_current()) then
+        gui.set_color(self.vh.bg, COLORS.CURRENT)
+    else
+        gui.set_color(self.vh.bg, opened and COLORS.OPENED or COLORS.CLOSED)
+    end
+    self.views.btn:set_ignore_input(not opened)
 end
 
 function LevelCell:percent_set(percent)
